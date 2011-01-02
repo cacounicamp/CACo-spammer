72ac909030a943a3ea08703c5db18fa9459d76be
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2011-01-02 19:35:03 -0200

    Implementada leitura a partir de arquivos de configuração

--- spammer.py
+++ spammer.py
@@ -21,6 +21,7 @@ import sys
 from getopt import gnu_getopt as getopt
 from smtplib import SMTP
 import email
+import ConfigParser
 
 # Classes
 class Email(SMTP):
@@ -66,7 +67,7 @@ def lista_de_alunos():
 # inciando execução do script
 
 # string com a ajuda (deve ser impressa com a opção -h ou --help)
-help_string =u"\
+help_string = u"\
 Uso: spammer [opções] arquivo_da_mensagem\n\
 Opções:\n\
 -a, --all    : especifica envio para todos os alunos do IC\n\
@@ -81,6 +82,19 @@ Opções:\n\
 shortopts_string = "ahvqy:c:t:n:"
 longopts_string = ["all", "help", "verbose", "quiet", "ano=", "curso=", "titulo=", "anexo="]
 
+# FIXME: realizar a leitura de senha durante a execução, evitando armazenamento
+# em arquivo
+# lendo arquivo de configuração
+# Exemplo:
+# [defaults]
+# host = hostname
+# login = login_no_servidor_smtp
+# user = usuario_no_servidor_smtp
+# port = porta_de_conexao_ao_servidor
+# passwd = senha
+config = ConfigParser.ConfigParser()
+config.read("/etc/caco_spammer.conf", os.path.expandvars("${HOME}/.caco_spammer.conf"))
+# lendo parâmetros de linha de comando
 opts,file_list = getopt(sys.argv[1:], shortopts_string, longopts_string)
 # TODO: buscar um jeito de fazer as opções longas se referirem às curtas (ou vice versa)
 opts = dict(opts)
@@ -115,18 +129,19 @@ elif opts.has_key("--ano"):
 
 # TODO: implementar a filtragem por curso
 
+# FIXME: parar de usar as variáveis inúteis abaixo
 # TODO: implementar a leitura a partir de um arquivo de configuração
-caco = "caco@ic.unicamp.br"
-host = "teste"
-login = "teste"
-user = "teste"
-port = 42
-passwd = "teste"
+CACO = "caco@ic.unicamp.br"
+host = config.get("defaults", "host")
+login = config.get("defaults", "login")
+user = config.get("defaults", "user")
+port = config.get("defaults", "port")
+passwd = config.get("defaults", "passwd")
 
 email_list = []
 for aluno in student_list:
-    email_list.append(Email(aluno, caco, titulo, file_list[0], anexos,
+    email_list.append(Email(aluno, CACO, titulo, file_list[0], anexos,
                             login, user, host, port, passwd))
 
 # enviando lista
-map (lambda x: x.spam(), email_list)
+map(lambda x: x.spam(), email_list)
